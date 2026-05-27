8866905f449f8a457e8de54905b076a534fa6852
nishok561-coder <nishok561@gmail.com>
2026-05-28 00:54:19 +0530

    made a working model

--- TaskManagerWeb/wwwroot/js/site.js
+++ TaskManagerWeb/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const glow = document.querySelector(".cursor-glow");
 
-// Write your JavaScript code.
+document.addEventListener("mousemove", (e) => {
+
+    glow.style.left = `${e.clientX}px`;
+
+    glow.style.top = `${e.clientY}px`;
+
+});
+const toggleBtn =
+document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+});
